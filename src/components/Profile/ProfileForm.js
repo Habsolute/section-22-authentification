8b7fe6bdf7f76c120961166d1e97d72d49958d59
Habsolute <85709216+Habsolute@users.noreply.github.com>
2022-06-16 10:53:22 -0400

    Setup profil form

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,27 @@
-import classes from './ProfileForm.module.css';
+import { useRef } from "react";
+import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+
+  function submitHandler(e) {
+    e.prevent.default();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    // add validation
+  }
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input ref={newPasswordInputRef} type="password" id="new-password" />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
